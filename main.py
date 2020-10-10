0eda1a7ffd5761c5a989a46b4c1985fc9ce8aefc
thewarsawpakt <smallperson07@gmail.com>
2020-10-10 18:07:31 -0500

    Complete redesign of the project

--- main.py
+++ main.py
@@ -1,11 +1,1 @@
-from paktgame.window import Game
-from paktgame.player import Player
 
-import pyglet
-import os
-
-window = Game(640, 480, Player(640//2, 480//2), "PaktGame")
-
-# from paktgame.events import *
-
-window.run()
